504dea5c91ba431a43214bf19176d701e3686795
Makstorm <mxm.horobets@gmail.com>
2023-09-25 20:26:16 +0300

    done calc practice

--- src/route/calc.js
+++ src/route/calc.js
@@ -4,12 +4,20 @@ const router = express.Router()
 
 // Підключіть файли роутів
 const test = require('./test')
-const calc = require('./calc')
 // Підключіть інші файли роутів, якщо є
 
 // Об'єднайте файли роутів за потреби
-router.use('/', test)
-router.use('/calc', calc)
+router.get('/', (req, res) => {
+  res.render('calc', {
+    name: 'calc',
+
+    component: [],
+
+    title: 'Calc',
+
+    data: {},
+  })
+})
 // Використовуйте інші файли роутів, якщо є
 
 // Експортуємо глобальний роутер
